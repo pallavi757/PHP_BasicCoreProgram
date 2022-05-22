0b4a2e7240717c640f5f6b189450d469d5eb208b
unknown <kinekarpallavi21@gmail.com>
2022-05-22 15:49:18 +0530

    [Pallavi]Add:To find quotient and reminder

---
+++ quotientreminder.php
@@ -0,0 +1,9 @@
+<?php
+$dividend=0;
+$divisor=0;
+$dividend=readline("Enter dividend:");
+$divisor=readline("Enter divisor:");
+$quotient = $dividend / $divisor;
+echo "Quotient is:".$quotient . "\n";
+$reminder = $dividend % $divisor;
+echo "Reminder is:".$reminder;
